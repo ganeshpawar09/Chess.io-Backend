c567095b483664fa8a6fdc55065c39a176f102da
Ganesh Pawar <gapawar2003@gmail.com>
2024-02-17 10:29:35 +0530

    1

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import cors from "cors";
+import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+
 dotenv.config({
   path: "C:/Users/gapaw/Desktop/Online Chess/onlinechess_backend/.env",
 });
@@ -11,17 +12,13 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: process.env.CORS_ORIGIN || "*", // Provide a default value
   },
-});
-
-server.listen(process.env.PORT || 8000, () => {
-  console.log(`Server is listening on port ${process.env.PORT || 8000}`);
 });
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || "*", // Provide a default value
     credentials: true,
   })
 );
@@ -29,32 +26,39 @@ app.use(
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
 
-  socket.on("join_room", async ({ roomName }) => {
+  socket.on("join_room", ({ roomName }) => {
     try {
       socket.join(roomName);
       socket.broadcast.to(roomName).emit("newMemberInChat");
-      console.log(`${socket.id} is connected to ${roomName    }`);
+      console.log(`${socket.id} is connected to ${roomName}`);
     } catch (error) {
-      console.error("Error updating user:", error);
-    }
-  });
-  socket.on("sendNewBoard", async ({ roomName, chessBoard, color }) => {
-    try {
-      socket.broadcast.to(roomName).emit("newBoard", { chessBoard, color });
-    } catch (error) {
-      console.error("Error storing message:", error);
+      console.error("Error joining room:", error);
     }
   });
 
-  socket.on("user_disconnect", async ({ roomName }) => {
+  socket.on("sendNewBoard", ({ roomName, chessBoard, color }) => {
+    try {
+      socket.broadcast.to(roomName).emit("newBoard", { chessBoard, color });
+    } catch (error) {
+      console.error("Error sending new board:", error);
+    }
+  });
+
+  socket.on("user_disconnect", ({ roomName }) => {
     try {
       socket.leave(roomName);
     } catch (error) {
-      console.error("Error updating user:", error);
+      console.error("Error disconnecting user:", error);
     }
   });
 
   socket.on("disconnect", () => {
     console.log(`Socket id: ${socket.id} is disconnected`);
   });
+});
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
